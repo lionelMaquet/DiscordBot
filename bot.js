d0c2c0892eabe1f0609825d134c6c7ab448696c6
Lionel Maquet <lionel.maquet@gmail.com>
2020-12-02 15:54:03 +0100

    adds basics

---
+++ bot.js
@@ -0,0 +1,16 @@
+require("dotenv").config()
+
+const Discord = require("discord.js")
+const client = new Discord.Client
+
+client.on('ready', () => {
+    console.log('BlezBot est prêt !!!')
+})
+
+client.on('message', msg => {
+    if (msg.content === "ping"){
+        msg.reply("pong")
+    }
+})
+
+client.login(process.env.BOT_TOKEN)
